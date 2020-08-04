b0a4c02836d9708d267376b143baa8a465516f7b
Kim Paulissen <kim.paulissen@kuleuven.be>
2020-08-04 22:59:04 +0200

    add .env for api_key
    yes I know

--- src/_data/characters.js
+++ src/_data/characters.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const axios = require("axios");
 
 module.exports = async function () {
   try {
     const response = await axios.get(
-      "https://www.potterapi.com/v1/characters?key=$2a$10$mkKLbPJea94VvwYR5YVFU.Kj/rf3IvuGy6.ajVPOlyBsumszwk3/S"
+      `https://www.potterapi.com/v1/characters?key=${process.env.API_KEY}`
     );
     // console.log(response.data);
     return response.data;
