9bace2f7ebac1eca4f1f748bdd163ea89e25ca18
Kim Paulissen <kim.paulissen@kuleuven.be>
2020-08-04 23:09:20 +0200

    more specific variable name for api key

--- src/_data/characters.js
+++ src/_data/characters.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `https://www.potterapi.com/v1/characters?key=${process.env.API_KEY}`
+      `https://www.potterapi.com/v1/characters?key=${process.env.POTTER_API_KEY}`
     );
     // console.log(response.data);
     return response.data;
